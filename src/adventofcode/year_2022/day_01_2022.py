187f91c5a73d0304ab69d3deb0eeda63cf50fd66
Alexdruso <alessandro.sanvito@gmail.com>
2022-12-01 14:09:55 +0100

    feat: update

--- src/adventofcode/year_2022/day_01_2022.py
+++ src/adventofcode/year_2022/day_01_2022.py
@@ -1,43 +1,17 @@
-from adventofcode.registry.decorators import register_solution
+
+
 from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.registry.decorators import register_solution
 from adventofcode.util.input_helpers import get_input_for_day
 
-
-def find_most_calories(input_data: list[str]) -> int:
-    """Find the most calories a single Elf carries"""
-    calories = 0
-    max_calories = 0
-
-    for row in input_data + ['']:
-        if row == '':
-            max_calories = calories if calories > max_calories else max_calories
-            calories = 0
-            continue
-
-        calories += int(row)
-
-    return max_calories
-
-
-def find_top_three_most_calories(input_data: list[str]) -> int:
-    """Find the sum of calories of the top 3 Elves"""
-    calories = 0
-    chunks = []
-
-    for row in input_data + ['']:
-        if row == '':
-            chunks.append(calories)
-            calories = 0
-            continue
-        calories += int(row)
-
-    chunks.sort(reverse=True)
-    return sum(chunks[:3])
+from itertools import groupby
 
 
 @register_solution(2022, 1, 1)
 def part_one(input_data: list[str]):
-    answer = find_most_calories(input_data)
+    input_data = [list(group) for k, group in groupby(input_data, lambda x: x == "") if not k]
+    input_data = list(map(lambda elf: list(map(int, elf)), input_data))
+    answer = max(map(sum, input_data))
 
     if not answer:
         raise SolutionNotFoundException(2022, 1, 1)
@@ -47,7 +21,9 @@ def part_one(input_data: list[str]):
 
 @register_solution(2022, 1, 2)
 def part_two(input_data: list[str]):
-    answer = find_top_three_most_calories(input_data)
+    input_data = [list(group) for k, group in groupby(input_data, lambda x: x == "") if not k]
+    input_data = list(map(lambda elf: list(map(int, elf)), input_data))
+    answer = sum(sorted(list(map(sum, input_data)))[-3:])
 
     if not answer:
         raise SolutionNotFoundException(2022, 1, 2)
