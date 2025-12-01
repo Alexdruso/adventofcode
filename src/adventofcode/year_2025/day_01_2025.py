d7393e819b2eac0771fd59f2a3e07ba61fd1d8cd
Alessandro <36210143+Alexdruso@users.noreply.github.com>
2025-12-01 20:28:01 +0000

    feat: part 1

--- src/adventofcode/year_2025/day_01_2025.py
+++ src/adventofcode/year_2025/day_01_2025.py
@@ -1,16 +1,24 @@
 
 
-from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.exceptions import SolutionNotFoundError
 from adventofcode.registry.decorators import register_solution
 from adventofcode.util.input_helpers import get_input_for_day
+from itertools import accumulate
 
 
 @register_solution(2025, 1, 1)
 def part_one(input_data: list[str]):
-    answer = ...
+
+    answer = ((-1 if line[0] == "L" else 1)*int(line[1:]) for line in input_data)
+
+    answer = accumulate(answer, initial=50)
+    
+    answer = (element%100 for element in answer)
+
+    answer = len([element for element in answer if element == 0])
 
     if not answer:
-        raise SolutionNotFoundException(2025, 1, 1)
+        raise SolutionNotFoundError(2025, 1, 1)
 
     return answer
 
@@ -20,7 +28,7 @@ def part_two(input_data: list[str]):
     answer = ...
 
     if not answer:
-        raise SolutionNotFoundException(2025, 1, 2)
+        raise SolutionNotFoundError(2025, 1, 2)
 
     return answer
 
