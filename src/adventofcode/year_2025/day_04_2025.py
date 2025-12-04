31dd6a69e91f53a80cbe1871a66e3440a209301e
Alessandro <36210143+Alexdruso@users.noreply.github.com>
2025-12-04 18:45:18 +0000

    feat: day 4 part 1

--- src/adventofcode/year_2025/day_04_2025.py
+++ src/adventofcode/year_2025/day_04_2025.py
@@ -5,7 +5,25 @@ from adventofcode.util.input_helpers import get_input_for_day
 
 @register_solution(2025, 4, 1)
 def part_one(input_data: list[str]):
-    answer = ...
+    answer = 0
+
+    for x_idx, line in enumerate(input_data):
+        for y_idx, element in enumerate(line):
+            if element == "@":
+                surrounding_papers = 0
+
+                for dx in [-1, 0, 1]:
+                    for dy in [-1, 0, 1]:
+                        if (
+                            0 <= x_idx + dx < len(input_data)
+                            and 0 <= y_idx + dy < len(line)
+                            and not (dx == 0 and dy == 0)
+                            and input_data[x_idx + dx][y_idx + dy] == "@"
+                        ):
+                            surrounding_papers += 1
+
+                if surrounding_papers < 4:
+                    answer += 1
 
     if not answer:
         raise SolutionNotFoundError(2025, 4, 1)
