5761b325f64d37ea73a7e83791c467d68f9955b9
Alessandro <36210143+Alexdruso@users.noreply.github.com>
2025-12-05 20:12:15 +0000

    feat: part 1

---
+++ src/adventofcode/year_2025/day_05_2025.py
@@ -0,0 +1,44 @@
+from adventofcode.util.exceptions import SolutionNotFoundError
+from adventofcode.registry.decorators import register_solution
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+@register_solution(2025, 5, 1)
+def part_one(input_data: list[str]):
+    ranges = []
+
+    for index, line in enumerate(input_data):
+        if line.strip() == "":
+            break
+        start, end = line.split("-")
+        ranges.append((int(start), int(end) + 1))
+
+    def is_in_ranges(num):
+        return any(start <= num < end for start, end in ranges)
+
+    answer = sum(
+        1
+        for ingredient in input_data[index + 1 :]
+        if is_in_ranges(int(ingredient.strip()))
+    )
+
+    if not answer:
+        raise SolutionNotFoundError(2025, 5, 1)
+
+    return answer
+
+
+@register_solution(2025, 5, 2)
+def part_two(input_data: list[str]):
+    answer = ...
+
+    if not answer:
+        raise SolutionNotFoundError(2025, 5, 2)
+
+    return answer
+
+
+if __name__ == "__main__":
+    data = get_input_for_day(2025, 5)
+    part_one(data)
+    part_two(data)
