fcde5b212ca0288ec57f7a7426e58323f13cc41c
Alessandro <36210143+Alexdruso@users.noreply.github.com>
2025-12-11 19:19:13 +0000

    feat: res

--- src/adventofcode/year_2025/day_11_2025.py
+++ src/adventofcode/year_2025/day_11_2025.py
@@ -2,25 +2,55 @@ from adventofcode.util.exceptions import SolutionNotFoundError
 from adventofcode.registry.decorators import register_solution
 from adventofcode.util.input_helpers import get_input_for_day
 
+import networkx as nx
+
 
 @register_solution(2025, 11, 1)
 def part_one(input_data: list[str]):
-    answer = ...
+    G = nx.DiGraph()
 
-    if not answer:
+    for line in input_data:
+        if not line.strip():
+            continue
+        left, right = line.split(":")
+        src = left.strip()
+        targets = right.strip().split()
+        for dst in targets:
+            G.add_edge(src, dst)
+
+    try:
+        count = sum(1 for _ in nx.all_simple_paths(G, source="you", target="out"))
+    except nx.NetworkXNoPath:
         raise SolutionNotFoundError(2025, 11, 1)
 
-    return answer
+    if count is None:
+        raise SolutionNotFoundError(2025, 11, 1)
+
+    return count
 
 
 @register_solution(2025, 11, 2)
 def part_two(input_data: list[str]):
-    answer = ...
+    G = nx.DiGraph()
 
-    if not answer:
-        raise SolutionNotFoundError(2025, 11, 2)
+    for line in input_data:
+        if not line.strip():
+            continue
+        left, right = line.split(":")
+        src = left.strip()
+        targets = right.strip().split()
+        for dst in targets:
+            G.add_edge(src, dst)
 
-    return answer
+    result = (set(path) for path in nx.all_simple_paths(G, source="svr", target="out"))
+
+    result = (1 for path in result if ("dac" in path and "fft" in path))
+    result = sum(result)
+
+    if result is None:
+        raise SolutionNotFoundError(2025, 11, 1)
+
+    return result
 
 
 if __name__ == "__main__":
